68bcd7298a55ca1a9aaa266aa65b1286ce362ee5
WaseurGIT <waseurrahman31@gmail.com>
2025-12-29 20:01:57 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="text-xl font-semibold text-indigo-500">
+        Online Study Group
+      </h1>
+    </div>
+  );
+}
+
+export default App;
